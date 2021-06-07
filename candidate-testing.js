f8842eb969424963472403ff98ebd131ce3cba7c
Steller <>
2021-06-07 18:03:55 +0000

    Finding nnumber of correct answers

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,23 +8,25 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [ 'Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers= ["Sally Ride", "true", 40, "Trajectory", 3];
+let numCorrect
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
- candidateName =  input.question("What is your name? ")
 
+ candidateName =  input.question("What is your name? ")
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
- candidateAnswer = input.question(question)
-
+ for (i = 0; i < 5; i++){
+     candidateAnswers.push(input.question(questions[i]));
+ }
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -37,8 +39,18 @@ if (candidateAnswer.toLowercase === correctAnswer.toLowercase){
     console.log("You are wrong.")
 }
 
+ for (i=0; i < correctAnswers.length; i++){
+        if (typeof(correctAnswers[i]) === 'string' &&  candidateAnswers[i].toLowerCase === correctAnswers[i].toLowerCase){
+              numCorrect = numCorrect + 1;
+        } else if (typeof(correctAnswers[i]) === 'number' &&  Number(candidateAnswers[i]) === correctAnswers[i]){
+            numCorrect = numCorrect + 1
+        }
+ }
 
-  let grade;
+  let grade = function(i){
+      
+      
+  };
   
 
   return grade;
@@ -51,7 +63,6 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
-
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
